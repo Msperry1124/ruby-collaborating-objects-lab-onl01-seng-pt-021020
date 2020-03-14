debd87fe9797377163711180ed920726c3761409
Melissa Sperry <sperry.mel@gmail.com>
2020-03-14 04:02:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,22 @@
-class Song 
+class Song
   @@all = []
-  attr_accessor :name, :artist  
+  attr_accessor :name, :artist
   def initialize(name)
-    @name = name 
-    @@all << self 
-   end 
+    @name = name
+    @@all << self
+   end
    def self.new_by_filename(file)
      song_name = file.split(" - ")[1]
      new_artist_name = file.split(" - ")[0]
      new_song = Song.new(song_name)
      new_song.artist_name = new_artist_name
      new_song
-  end 
+  end
    def artist_name=(name)
      self.artist = Artist.find_or_create_by_name(name)
      self.artist.add_song(self)
-   end 
+   end
    def self.all
      @@all
-   end 
+   end
 end 
